5e939f8cf6c6be3c13fe9694b22f227e0990196c
Luna Futures <luna.futures+github@gmail.com>
2023-05-20 01:10:24 -0500

    Add a default box at 100, 100

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+interface TodoProps {
+  x: number;
+  y: number;
+}
+function Todo({ x, y }: TodoProps) {
+  return (
+    <div className="TodoBox" style={{ top: x, left: y }}>
+      Box
+    </div>
+  );
+}
+
 function App() {
+  const [todos, setTodos] = React.useState<TodoProps[]>([{ x: 100, y: 100 }]);
+
   return (
     <div className="App">
       <div className="AppHeader">Header</div>
@@ -24,6 +38,9 @@ function App() {
         style={{ background: "lightblue", height: "auto" }}
       >
         Body
+        {todos.map((todo) => (
+          <Todo x={todo.x} y={todo.y} />
+        ))}
       </div>
       <div className="AppFooter">Footer</div>
     </div>
