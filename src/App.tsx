8d6b994119c8429b67710d9c4f6d711cc692aa1b
Luna Futures <luna.futures+github@gmail.com>
2023-05-21 17:39:52 -0500

    Use useReducer for todo state

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,30 @@ import logo from "./logo.svg";
 import "./App.css";
 
 interface TodoProps {
+  id: number;
+  x: number;
+  y: number;
+  zIndex: number;
+  moveToTop: (id: number) => void;
+}
+
+interface AllTodosState {
+  todos: TodoProps[];
+  zIndexMax: number;
+  idMax: number;
+}
+interface MoveToTopAction {
+  type: "moveToTop";
+  id: number;
+}
+interface CreateAction {
+  type: "create";
   x: number;
   y: number;
 }
-function Todo({ x, y }: TodoProps) {
+type TodoReducerAction = MoveToTopAction | CreateAction;
+
+function Todo({ id, x, y, zIndex, moveToTop }: TodoProps) {
   const initialWidth = 150;
   return (
     <div
@@ -16,8 +36,12 @@ function Todo({ x, y }: TodoProps) {
         top: y - initialWidth / 2,
         width: initialWidth,
         minHeight: initialWidth,
+        zIndex,
       }}
-      onClick={(event) => event.stopPropagation()}
+      onClick={(event) => {
+        moveToTop(id);
+        event.stopPropagation();
+      }}
     >
       {x}, {y}
     </div>
@@ -25,14 +49,55 @@ function Todo({ x, y }: TodoProps) {
 }
 
 function App() {
-  const [todos, setTodos] = React.useState<TodoProps[]>([{ x: 100, y: 100 }]);
+  const initialTodoState: AllTodosState = { todos: [], zIndexMax: 0, idMax: 0 };
+  const [todos, dispatchTodos] = React.useReducer(
+    todoReducer,
+    initialTodoState
+  );
+
+  function moveToTop(id: number): void {
+    dispatchTodos({ type: "moveToTop", id });
+  }
+
+  function todoReducer(
+    previous: AllTodosState,
+    action: TodoReducerAction
+  ): AllTodosState {
+    const nextZ = previous.zIndexMax + 1;
+    switch (action.type) {
+      case "moveToTop":
+        return {
+          idMax: previous.idMax,
+          zIndexMax: nextZ,
+          todos: previous.todos.map((todo) =>
+            todo.id === action.id ? { ...todo, zIndex: nextZ } : todo
+          ),
+        };
+      case "create":
+        const nextId = previous.idMax + 1;
+        return {
+          idMax: nextId,
+          zIndexMax: nextZ,
+          todos: [
+            ...previous.todos,
+            {
+              id: nextId,
+              x: action.x,
+              y: action.y,
+              zIndex: nextZ,
+              moveToTop: (id) => moveToTop(id),
+            },
+          ],
+        };
+    }
+  }
 
   return (
     <div className="App">
       <div className="AppHeader">Header</div>
       <div
         className="AppBody"
-        onMouseMove={(e) => {
+        onClick={(e) => {
           const q = {
             pageX: e.pageX,
             pageY: e.pageY,
@@ -43,28 +108,18 @@ function App() {
             offsetX: e.nativeEvent.offsetX,
             offsetY: e.nativeEvent.offsetY,
           };
-          // console.log(q);
-        }}
-        onClick={(e) => {
-          const q = {
-            pageX: e.pageX,
-            pageY: e.pageY,
-            screenX: e.screenX,
-            screenY: e.screenY,
-            clientX: e.clientX, // Point in browser
-            clientY: e.clientY,
-            offsetX: e.nativeEvent.offsetX, // Point relative to parent
-            offsetY: e.nativeEvent.offsetY,
-          };
           console.log(q);
-          setTodos((prev) => [...prev, { x: q.offsetX, y: q.offsetY }]);
-          // setTodos((prev) => [...prev, { x: q.clientX, y: q.clientY }]);
-          console.log("event.target:", e.target);
+          dispatchTodos({ type: "create", x: q.offsetX, y: q.offsetY });
         }}
       >
-        Body
-        {todos.map((todo) => (
-          <Todo x={todo.x} y={todo.y} />
+        {todos.todos.map((todo) => (
+          <Todo
+            x={todo.x}
+            y={todo.y}
+            zIndex={todo.zIndex}
+            id={todo.id}
+            moveToTop={() => moveToTop(todo.id)}
+          />
         ))}
       </div>
       <div className="AppFooter">Footer</div>
