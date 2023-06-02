e523696bf7088f2efe546239761f5861cfd90e07
Luna Futures <luna.futures+github@gmail.com>
2023-06-01 23:16:01 -0500

    Delete unused

--- src/demo-component.tsx
+++ src/demo-component.tsx
@@ -1,20 +1,4 @@
 export function DemoComponent() {
-  function click(name: string, e: React.MouseEvent) {
-    console.log(
-      `${name}: target ${(e.target as HTMLElement).id} currentTarget ${
-        e.currentTarget.id
-      }`
-    );
-  }
-
-  function key(name: string, e: React.KeyboardEvent) {
-    console.log(name, e.key);
-  }
-
-  const styling: React.CSSProperties = {
-    border: "1px solid white",
-    padding: "10px",
-  };
   return (
     <div style={{ background: "black", color: "white", height: "100vh" }}>
       <div className="demo-box" style={{ transform: "" }}>
